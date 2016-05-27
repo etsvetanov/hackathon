11510bc06c37ec665b661f1fd342a83a33d256c6
Evgeni <ecvetanov@gmail.com>
2016-05-28 01:10:10 +0300

    Team list view and controller

---
+++ app/www/js/list.controller.js
@@ -0,0 +1,19 @@
+angular.module('starter.controllers', [])
+
+    .controller('ListCtrl', function ($scope) {
+        $scope.shouldShowDelete = false;
+        $scope.shouldShowReorder = false;
+        $scope.listCanSwipe = true;
+        $scope.items = [
+            'Movies',
+            'Geography',
+            'Programming'
+        ];
+
+        $scope.func = function () {
+            debugger;
+        }
+    });
+
+
+
