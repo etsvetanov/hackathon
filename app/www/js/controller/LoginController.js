9db5432f5cf950c991ad8b767e189ec4e3fac2db
Linh <litozor@gmail.com>
2016-05-28 22:11:33 +0300

    style changes

--- app/www/js/controller/LoginController.js
+++ app/www/js/controller/LoginController.js
@@ -3,8 +3,8 @@ angular.module('starter.controllers')
 .controller('LoginCtrl', function($scope, $q, $timeout, sendFromLogin, $rootScope, $state) {
   $scope.username = "";
   $rootScope.username;
-  $scope.login = function() {
-    sendFromLogin.Login($scope.username)
+  $scope.login = function(name) {
+    sendFromLogin.Login(name)
       .then(function successCallback(response) {
           $rootScope.username = response.data.Username;
           console.log($rootScope.username);
