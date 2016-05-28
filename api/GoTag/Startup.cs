d57053cef45be59dbe0da85d8cce21ff63dbfd4b
dzhelev <maverikkc@abv.bg>
2016-05-29 00:37:30 +0300

    styles

--- api/GoTag/Startup.cs
+++ api/GoTag/Startup.cs
@@ -15,21 +15,22 @@ namespace GoTag
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            app.Map("/signalr", map =>
-            {
-               map.UseCors(CorsOptions.AllowAll);
-                var hubConfiguration = new HubConfiguration
-                {
-                    // You can enable JSONP by uncommenting line below.
-                    // JSONP requests are insecure but some older browsers (and some
-                    // versions of IE) require JSONP to work cross domain
-                    EnableJSONP = true
-                };
-                // Run the SignalR pipeline. We're not using MapSignalR
-                // since this branch already runs under the "/signalr"
-                // path.
-                map.RunSignalR(hubConfiguration);
-            });
+            app.MapSignalR();
+            //app.Map("/signalr", map =>
+            //{
+            //    map.UseCors(CorsOptions.AllowAll);
+            //    var hubConfiguration = new HubConfiguration
+            //    {
+            //         You can enable JSONP by uncommenting line below.
+            //         JSONP requests are insecure but some older browsers (and some
+            //         versions of IE) require JSONP to work cross domain
+            //        EnableJSONP = true
+            //    };
+            //    Run the SignalR pipeline. We're not using MapSignalR
+            //     since this branch already runs under the "/signalr"
+            //     path.
+            //    map.RunSignalR(hubConfiguration);
+            //});
         }
     }
 }
