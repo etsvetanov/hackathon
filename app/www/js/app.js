38222f014eb2b459399affc2fa7ce324f9c3e813
Linh <litozor@gmail.com>
2016-05-28 12:18:45 +0300

    redirecting to main state when no hash is present

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -1,9 +1,14 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+.run(function($ionicPlatform, $rootScope) {
+	$ionicPlatform.ready(function() {
+		if(window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+});
+
+
+if(!window.location.hash) {
+	window.location.hash = '#/';
+}
